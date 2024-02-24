feea260ebc217ee20785854669256c1b7718476c
SahilTyagii <tyagi28sahil@gmail.com>
2024-02-24 16:01:41 +0530

    doing object detection on video

--- object_detection.py
+++ object_detection.py
@@ -31,3 +31,20 @@ def detect(frame, net, transform):
             cv2.putText(frame, labelmap[i-1], (int(pt[0]), int(pt[1])), cv2.FONT_HERSHEY_SIMPLEX, 2, (255, 255, 255), 2, cv2.LINE_AA)
             j += 1
     return frame
+
+# Creating the SSD neural network
+net = build_ssd('test')
+net.load_state_dict(torch.load('ssd300_mAP_77.43_v2.pth', map_location = lambda storage, loc: storage))
+
+# Creating the transformation
+transform = BaseTransform(net.size, (104/256.0, 117/256.0, 123/256.0))
+
+# Doing some object detection on a video
+reader = imageio.get_reader('funny_dog.mp4')
+fps = reader.get_meta_data()['fps']
+writer = imageio.get_writer('output.mp4', fps = fps)
+for i, frame in enumerate(reader):
+    frame = detect(frame, net.eval(), transform)
+    writer.append_data(frame)
+    print(i)
+writer.close()
